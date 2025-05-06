1a458a6bf2d26065eac2620c31576eb2e0965897
CLQ060728 <liu.qian.pro@gmail.com>
2025-05-06 04:36:02 +0200

    change depth map color map to 'turbo' in waymo and nuscenes.py

--- datasets/nuscenes.py
+++ datasets/nuscenes.py
@@ -713,7 +713,7 @@ class NuScenesDataset(SceneDataset):
             if "depth_maps" in data_dict:
                 all_depth_maps = np.array([])
                 # convert depth maps for visualisation
-                cmap = matplotlib.colormaps.get_cmap('Spectral_r')
+                cmap = matplotlib.colormaps.get_cmap('turbo') # 'Spectral_r'
                 for depth_map in data_dict["depth_maps"].cpu().numpy():
                     depth_map = depth_map * 255.0
                     depth_map = (cmap(depth_map)[:, :, :3] * 255.0)[:, :, ::-1]
