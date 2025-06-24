65b74545205d133b1ca582f3921f9bedb60ae287
CLQ060728 <liu.qian.pro@gmail.com>
2025-06-24 19:35:36 +0000

    revise sam2 installation script

--- install_sam2.sh
+++ install_sam2.sh
@@ -3,3 +3,15 @@ rm -rf ./.git
 pip install -e .
 mkdir ./checkpoints/
 wget -P ./checkpoints/ https://dl.fbaipublicfiles.com/segment_anything_2/092824/sam2.1_hiera_large.pt
+pip install --upgrade numpy==2.2.6
+pip install --upgrade scipy==1.15.3
+pip install --upgrade pandas==2.3.0
+pip install --upgrade scikit-learn==1.7.0
+pip install --upgrade matplotlib==3.10.3
+pip install --upgrade plotly==6.1.2
+pip install --upgrade seaborn==0.13.2
+pip install --upgrade torch==2.5.1
+pip install --upgrade torchvision==0.20.1
+pip install --upgrade tqdm==4.67.1
+pip install --upgrade pillow==11.2.1
+pip install --upgrade opencv-python==4.11.0.86
