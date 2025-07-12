ae7b44137fa4750c73ff50e5ae6edfdb7806ccef
Qian Liu <liu.qian.pro@gmail.com>
2025-07-12 16:30:52 +0200

    resume to previous install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-pip install --default-timeout=1000 --upgrade -r requirements.txt
+pip install --upgrade -r requirements.txt
 pip install git+https://github.com/NVlabs/tiny-cuda-nn/#subdirectory=bindings/torch
 pip install git+https://github.com/nerfstudio-project/nerfacc.git@8340e19daad4bafe24125150a8c56161838086fa
 pip cache purge
