2e7df895cc40ec4c14dc87358eac1fba7374ace2
CLQ060728 <liu.qian.pro@gmail.com>
2025-07-12 20:17:32 +0000

    updating rampp related files

--- inference_ram_plus.py
+++ inference_ram_plus.py
@@ -28,7 +28,7 @@ parser.add_argument('--image-size',
                     default=384,
                     type=int,
                     metavar='N',
-                    help='input image size (default: 448)')
+                    help='input image size (default: 384)')
 parser.add_argument('--output',
                     metavar='DIR',
                     help='path to output tags',
