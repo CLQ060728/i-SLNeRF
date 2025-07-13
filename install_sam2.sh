0e7bc48350e08e15c3b43fe4045c6869f5c11f6e
CLQ060728 <liu.qian.pro@gmail.com>
2025-07-13 13:07:25 +0000

    change numpy to 1.24.4 for gdino-1.5

--- install_sam2.sh
+++ install_sam2.sh
@@ -18,8 +18,8 @@ pip install --upgrade torch==2.5.1
 pip install --upgrade torchvision==0.20.1
 pip install --upgrade xformers==0.0.28.post3
 pip install --upgrade tqdm==4.67.1
-pip install --upgrade pillow==11.2.1
-pip install --upgrade numpy==1.26.4
+pip install --upgrade pillow==10.2.0
+pip install --upgrade numpy==1.24.4
 pip install --upgrade scipy==1.15.3
 pip install --upgrade pandas==2.3.0
 pip install --upgrade scikit-learn==1.7.0
