6357b3a63f6afdb9b954e21bf8a05498bc7259b8
Qian Liu <liu.qian.pro@gmail.com>
2025-07-20 22:07:21 +0200

    update pip install torch==2.1.0 torchvision==0.16.0  --index-url https://download.pytorch.org/whl/cu121

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-pip install --upgrade torch==2.1.0
+pip install torch==2.1.0 torchvision==0.16.0 --index-url https://download.pytorch.org/whl/cu121
 pip install --upgrade -r requirements.txt
 pip install git+https://github.com/NVlabs/tiny-cuda-nn/#subdirectory=bindings/torch
 pip install git+https://github.com/nerfstudio-project/nerfacc.git@8340e19daad4bafe24125150a8c56161838086fa
