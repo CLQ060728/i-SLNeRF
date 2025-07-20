c6d12857a56770261bbbccd62571bc4e0ce427b1
Qian Liu <liu.qian.pro@gmail.com>
2025-07-20 21:55:25 +0200

    update install.sh, add torch 2.1.0 installation

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+--extra-index-url https://download.pytorch.org/whl/cu121
+pip install --upgrade torch==2.1.0
 pip install --upgrade -r requirements.txt
 pip install git+https://github.com/NVlabs/tiny-cuda-nn/#subdirectory=bindings/torch
 pip install git+https://github.com/nerfstudio-project/nerfacc.git@8340e19daad4bafe24125150a8c56161838086fa
